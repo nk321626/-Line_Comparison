99e851704b6ff97e0e261a414c1a15f2dc46498b
nk321626 <nk321626@gmail.com>
2022-05-11 17:46:23 +0530

    Added Line Comparison.cs to Claculate length of line -UC1

--- Line_Comparison/Program.cs
+++ Line_Comparison/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Line_Comparison;
+
 Console.WriteLine("Welcome to Line Comparison Computation Program");
+LineComparison lineComparison = new LineComparison();
+lineComparison.LineLength();
+Console.ReadLine();
